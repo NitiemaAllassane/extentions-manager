c0f47e95c47dce9cec2ba1c7e1592b91b4575fd6
Nitiema Allassane <allassanenitiema@gmail.com>
2026-05-26 22:57:04 +0000

    Add: (Add Navigation  component style and layout)

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,8 +1,13 @@
 function ActionButton({ text }: { text: string }) {
     return (
-        <button>
+        <button 
+            className="inline-block bg-white border border-neutral-100 
+            shadow-sm px-3 py-1 rounded-2xl cursor-pointer
+            text-neutral-800 font-normal"
+        >
             {text}
         </button>
     )
 }
+
 export default ActionButton;
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,9 +4,11 @@ import ActionButton from "./ActionButton";
 function Navigation() {
     return (
         <nav>
-            <div className="container">
-                <h1>Extensions List</h1>
-                <ul>
+            <div className="container flex items-center justify-between">
+                <h1 className="font-bold text-3xl text-neutral-800">
+                    Extensions List
+                </h1>
+                <ul className="flex items-center gap-4">
                     <ActionButton text="All"  />
                     <ActionButton text="Active"  />
                     <ActionButton text="Inactive"  />
