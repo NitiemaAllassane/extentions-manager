efde1489d201957c45df61ee76e4aa5cc0e79cfb
Nitiema Allassane <allassanenitiema@gmail.com>
2026-05-28 09:04:40 +0000

    (add): Add dark mode switching

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,13 +1,21 @@
-// import moonIcon from '../assets/images/icon-moon.svg';
+import moonIcon from '../assets/images/icon-moon.svg';
 import sunIcon from '../assets/images/icon-sun.svg';
+import { useDarkMode } from '../hooks/useDarkMode';
 
 function ThemeSwitcher() {
+    const { isDark, toggleDarkMode } = useDarkMode();
+
     return (
         <button 
             className='bg-neutral-100 p-2 rounded-md cursor-pointer hover:bg-neutral-300 
             dark:bg-neutral-700 dark:hover:bg-neutral-600'
+            onClick={toggleDarkMode}
         >
-            <img src={sunIcon} alt="Moon Icon" />
+            {isDark ? (
+                <img src={sunIcon} alt="Moon Icon" />
+            ) : (
+                <img src={moonIcon} alt="Moon Icon" />
+            )}
         </button>
     )
 }
---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export function useDarkMode() {
+  const [ isDark, setIsDark ] = useState(() => {
+    const saved = localStorage.getItem("theme");
+    if (saved) return saved === "dark";
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+  });
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", isDark);
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+  }, [isDark]);
+
+
+  const toggleDarkMode = () => setIsDark(prev => !prev);
+
+  return { isDark, toggleDarkMode };
+}
